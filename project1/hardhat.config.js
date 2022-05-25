916975b2061c626f12d8c447667ad7e1d529d77b
Reza Alizadeh Majd <r.a.majd@gmail.com>
2022-05-25 14:36:27 +0430

    add deployment script

--- project1/hardhat.config.js
+++ project1/hardhat.config.js
@@ -1,9 +1,20 @@
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
 
+require("dotenv").config();
+
+const privateKey = process.env.PRIVATE_KEY;
+const deployUrl = process.env.DEPLOY_URL;
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.8",
+  networks: {
+    rinkeby: {
+      url: deployUrl,
+      accounts: [`0x${privateKey}`],
+    },
+  },
 };
